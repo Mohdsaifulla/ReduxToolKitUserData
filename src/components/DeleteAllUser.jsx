e46b625da72ce1b5a96a8e2943a8aa32ed9c3600
Saif <saif49032@gmail.com>
2023-08-24 10:37:00 +0530

    User Detail Created

---
+++ src/components/DeleteAllUser.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DeleteAllUser = () => {
+  return (
+    <div>DeleteAllUser</div>
+  )
+}
+
+export default DeleteAllUser
--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
-
+import React from "react";
+import DeleteAllUser from "./DeleteAllUser";
+import styled from "styled-components";
 const UserDetails = () => {
   return (
-    <div>UserDetails</div>
-  )
-}
+    <Wrapper>
+      <div className="content">
+        <div className="admin-table">
+          <div className="admin-subtitle">List of user Details</div>
+          <button className="btn add-btn">Add New Users</button>
+        </div>
+        <ul>
+          <li>hi</li>
+          <li>hi</li>
+        </ul>
+        <hr />
+        <DeleteAllUser />
+      </div>
+    </Wrapper>
+  );
+};
+const Wrapper = styled.section`
+  margin: 1rem 3.2rem;
 
-export default UserDetails
+  .content ul {
+    list-style-type: none !important;
+    display: flex;
+    flex-direction: column;
+  }
+
+  h3 {
+    margin: 0;
+  }
+
+  .admin-table {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    margin: 4rem 0;
+  }
+
+  .admin-subtitle {
+    font-size: 3.2rem;
+  }
+
+  .delete-btn {
+    background-color: transparent;
+    border: none;
+  }
+
+  .delete-icon {
+    font-size: 2.6rem;
+    color: #f12711;
+    filter: drop-shadow(0.2rem 0.2rem 0.5rem rgb(255 0 0 / 0.2));
+    cursor: pointer;
+  }
+  @media screen and (max-width: 998px) {
+    .admin-subtitle {
+      margin-bottom: 1.6rem;
+    }
+  }
+`;
+
+export default UserDetails;
